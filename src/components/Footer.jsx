ffe0673e7361ab88de1a1055cba00ebcdde13900
ejjays <ejalloso021@gmail.com>
2026-01-11 06:10:59 +0800

    Update UI: Add SocialMedia component, refine icons, and remove unused files

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <p className="text-xs text-center px-2">
-      By continuing, you agree to <span className="underline cursor-pointer">terms & ethics of use</span>
-    </p>
-  );
-};
-
-export default Footer;

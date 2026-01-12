77664bcd0f8270e54843e14f3bae6b5b77b90f18
ejjays <christsonalloso021@gmail.com>
2026-01-13 00:47:23 +0800

    Update supported services

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react';
 import { Plus } from 'lucide-react';
-import ServicesModal from './modals/ServicesModal';
+import SupportedServices from './modals/SupportedServices';
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
-      <header 
-        className="flex gap-2 items-center justify-center p-4 cursor-pointer hover:opacity-80 transition-opacity"
+      <header
+        className='flex gap-2 items-center justify-center p-4 cursor-pointer hover:opacity-80 transition-opacity'
         onClick={() => setIsModalOpen(true)}
       >
-        <div className="bg-gray-800 p-1 rounded-full">
-          <Plus size={14}/>
+        <div className='bg-gray-800 p-1 rounded-full'>
+          <Plus size={14} />
         </div>
-        <h1 className="text-sm">supported services</h1>
+        <h1 className='text-sm'>supported services</h1>
       </header>
 
-      <ServicesModal 
-        isOpen={isModalOpen} 
-        onClose={() => setIsModalOpen(false)} 
+      <SupportedServices
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
       />
     </>
   );

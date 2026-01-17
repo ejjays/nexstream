99e659bee7d94d772e4daf9064cd3d87384bdbde
ejjays <christsonalloso021@gmail.com>
2026-01-17 23:22:55 +0800

    feat: animated maincontent button bg color

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import Header from './components/Header.jsx';
 import MainContent from './components/MainContent.jsx';
 import SocialMedia from './components/SocialMedia.jsx';
 
-{ /* javascript: (function () {
-  var script = document.createElement('script');
-  script.src = '//cdn.jsdelivr.net/npm/eruda';
-  document.body.appendChild(script);
-  script.onload = function () {
-    eruda.init();
-  };
-})(); */ }
 
 const App = () => {
   return (

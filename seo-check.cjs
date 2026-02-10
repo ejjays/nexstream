91b07d8028d407c4adda4b9ad189bf5fb0239b59
ejjays <ejalloso021@gmail.com>
2026-02-10 08:02:48 +0800

    fix: update SEO URLs from vercel to koyeb domain

---
+++ seo-check.cjs
@@ -0,0 +1,14 @@
+const SeoAnalyzer = require('seo-analyzer');
+
+new SeoAnalyzer()
+  .inputFiles(['index.html'])
+  .addRule('titleLengthRule', { min: 10, max: 60 })
+  .addRule('metaDescriptionRule', { min: 70, max: 160 })
+  .addRule('imgTagWithAltAttributeRule')
+  .addRule('aTagWithRelAttributeRule')
+  .addRule('canonicalTagRule')
+  .addRule('metaCustomTagRule', {
+    name: 'keywords',
+  })
+  .outputConsole()
+  .run();

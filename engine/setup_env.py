86d0c834101f80ddfa9b3c3eca0facf7e4bf0926
ejjays <christsonalloso021@gmail.com>
2026-05-11 13:33:27 +0800

    fix(engine): suppress false positive BAN-B310 security warning

--- engine/setup_env.py
+++ engine/setup_env.py
@@ -68,6 +68,7 @@ def bootstrap():
         fallback_url = "https://github.com/jayg996/BTC-ISMIR19/raw/master/test/btc_model_large_voca.pt"
         import urllib.request
         try:
+            # skipcq: BAN-B310
             with urllib.request.urlopen(fallback_url, timeout=30) as response, open(weights_path, 'wb') as out_file:
                 data = response.read()
                 out_file.write(data)
